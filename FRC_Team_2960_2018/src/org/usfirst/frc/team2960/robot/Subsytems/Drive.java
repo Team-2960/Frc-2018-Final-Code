1e962221369c49b168f070407c8ad24518f1af25
malcolmmachesky <malcolm9292@gmail.com>
2018-01-08 20:35:49 -0500

    First commit on driveCode branch added static instance method

--- FRC_Team_2960_2018/src/org/usfirst/frc/team2960/robot/Subsytems/Drive.java
+++ FRC_Team_2960_2018/src/org/usfirst/frc/team2960/robot/Subsytems/Drive.java
@@ -1,4 +1,24 @@
 package org.usfirst.frc.team2960.robot.Subsytems;
 
-public class Drive {
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Drive extends Subsystem{
+
+    private static Drive mInstance;
+
+
+    //static instance method
+    public static Drive instance()  {
+        if (mInstance == null) {
+            mInstance = new Drive();
+        }
+        return mInstance;
+    }
+
+
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
 }
