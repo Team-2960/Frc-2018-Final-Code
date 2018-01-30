ea201ca69d2045029c3cf6ae8c33855c6b537838
Malcolm Machesky <malcolm9292@gmail.com>
2018-01-30 12:36:58 -0500

    Added Led class
    Also worked on arduino code

--- Arduino Code For LEDS/FRC2018Aurdino/FRC2018Aurdino.ino
+++ Arduino Code For LEDS/FRC2018Aurdino/FRC2018Aurdino.ino
@@ -1,9 +1,27 @@
+#include <Wire.h>
+
+String Code = "not";
 void setup() {
   // put your setup code here, to run once:
 
+  Serial.begin(115200);  
+  Wire.begin(4);
+  Wire.onReceive(receiveLEDCode);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  delay(100);
 }
+
+void receiveLEDCode(int howMany){
+  while ( Wire.available() > 0) 
+  {
+    char n = (char)Wire.read();
+    if(((int)n)>((int)(' ')))
+    Code += n;
+  }
+
+  Serial.print(Code);
+}
+
