de833726601c7f3ae6c8a155497dedd10ee1e4de
Malcolm Machesky <malcolm9292@gmail.com>
2018-02-12 17:40:35 -0500

    Added Bolth switch autos

---
+++ FRC_Team_2960_2018/src/org/usfirst/frc/team2960/robot/Commands/Auto/LeftSwitchAuto.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team2960.robot.Commands.Auto;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class LeftSwitchAuto extends CommandGroup{
+
+    public LeftSwitchAuto() {
+
+    }
+}
---
+++ FRC_Team_2960_2018/src/org/usfirst/frc/team2960/robot/Commands/Auto/RightSwitchAuto.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team2960.robot.Commands.Auto;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class RightSwitchAuto extends CommandGroup{
+
+    public RightSwitchAuto() {
+
+    }
+
+}
